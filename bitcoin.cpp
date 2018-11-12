b35e3d1d54cecad331eee486528ea03d0f920c15
Martin Mizzell <martin@bravelantern.com>
2018-11-12 17:14:20 -0600

    update version and proto

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -80,7 +80,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/liberty-seeder:5.0.46/";
+    string ver = "/liberty-seeder:5.0.56/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
--- db.h
+++ db.h
@@ -12,7 +12,7 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 71021
+#define REQUIRE_VERSION 71025
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
